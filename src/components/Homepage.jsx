f1ad75ff5ce03c1590d021dd5db67f02b1cc9e30
Greg Hayes <gthelight@gmail.com>
2022-05-28 19:21:25 -0400

    Adds simplified view

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,10 +6,10 @@ import { useGetCryptosQuery } from '../services/cryptoApi'
 import Cryptocurrencies from './Cryptocurrencies'
 import News from './News'
 
-const { Title } = Typography
+const { Title } = Typography;
 
 const Homepage = () => {
-  const {data, isFetching} = useGetCryptosQuery();
+  const  { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats
   
   if(isFetching) return 'Loading...'
@@ -24,12 +24,12 @@ const Homepage = () => {
       <Col span={12}><Statistic title='Total 24hr Volume' value={millify(globalStats.total24hVolume)}/></Col>
       <Col span={12}><Statistic title='Total Markets' value={millify(globalStats.totalMarkets)}/></Col>
     </Row>
-    <div className="hoime-heading-container">
+    <div className="home-heading-container">
       <Title level={2} className="home-title">Top 10 Cryptocurrencies in the World</Title>
       <Title level={2} className="show-more"><Link to='/cryptocurrencies' >Show More</Link></Title>
     </div>
     <Cryptocurrencies simplified/>
-    <div className="hoime-heading-container">
+    <div className="home-heading-container">
       <Title level={2} className="home-title">Latest Crypto News</Title>
       <Title level={2} className="show-more"><Link to='/news'>Show More</Link></Title>
     </div>
